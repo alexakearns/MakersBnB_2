0d8dc20b763775fcc29440615e50bc3c99e941f4
tommyrharper <tommyrharper@gmail.com>
2020-04-21 10:56:22 +0100

    booking now has a space

--- jasmine-standalone-3.5.0/spec/bookingSpec.js
+++ jasmine-standalone-3.5.0/spec/bookingSpec.js
@@ -1,12 +1,27 @@
 describe('Booking', function() {
   var booking 
 
+  function Space() {
+  }
+  let space
+
   beforeEach(function() {
     booking = new Booking()
+    space = new Space
   })
 
   it('it has a reference', function() {
     expect(booking.reference).toEqual('reference')
   })
 
+
+
+  it('has a Space', function() {
+    expect(booking.setSpace(space)).toBeInstanceOf(Space)
+  }) 
+
+  it('has a Space', function(){
+    booking.setSpace(space)
+    expect(booking.space).toEqual(space)
+  })
 })
--- jasmine-standalone-3.5.0/src/booking.js
+++ jasmine-standalone-3.5.0/src/booking.js
@@ -1,3 +1,9 @@
 function Booking() {
   this.reference = "reference"
+  this.space;
+}
+
+Booking.prototype.setSpace = function(space) {
+  this.space = space;
+  return space;
 }
