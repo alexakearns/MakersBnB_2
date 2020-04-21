7dadcad4c3d55c5dfe1f65b023ddcb9ba7e841ea
Alex Lewis <alexlewis374@gmail.com>
2020-04-21 16:48:34 +0100

    add show listing details & confirm request

--- jasmine-standalone-3.5.0/src/landlordSignup.js
+++ jasmine-standalone-3.5.0/src/landlordSignup.js
@@ -8,13 +8,24 @@ $( "#submit-listing" ).click(function( event ) {
     var spaceID = $("#space-ID").val();
     var spaceDescription = $("#space-description").val();
     var dailyPrice = $("#daily-price").val();
-    var availableForm= $("#available-from").val();
+    var availableFrom= $("#available-from").val();
     var availableTo = $("#available-to").val();
-    space = new Space (spaceID, spaceDescription, dailyPrice, availableForm, availableTo);
+    space = new Space (spaceID, spaceDescription, dailyPrice, availableFrom, availableTo);
 
-    $("#this").text(spaceID + " " + spaceDescription + " " + dailyPrice + " " + availableForm + " " + availableTo)
-
+    landlord.listSpace(space)
+  
+    $("#Listing").text("Your new listing:")
+    $("#space").text("Space: " + spaceID)
+    $("#spacedec").text("Description: " + spaceDescription)
+    $("#dailyP").text("Price per night: " + dailyPrice)
+    $("#datesFrom").text("From: " + availableFrom)
+    $("#datesTo").text("To: " + availableTo)
   });
+
+  //navigates to confirmation page
+  $("#confirm-request").click(function(event) {
+    $("#request-confirmed").text("You have confirmed the request")
+  })
 
 
 
